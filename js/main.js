ceb97f4ae6744aa45075569f08fd5f1f17fd61ce
hmin1022 <leehyeonsnail@gmail.com>
2023-11-06 04:43:12 +0900

    반응형 레이아웃 작업

--- js/main.js
+++ js/main.js
@@ -1,28 +1,29 @@
 import ipads from '../data/ipads.js'
 import navigations from '../data/navigations.js'
 
-// Basket
+
+// 장바구니!
+// 장바구니 관련 요소 찾기.
 const basketStarterEl = document.querySelector('header .basket-starter')
 const basketEl = basketStarterEl.querySelector('.basket')
 
-basketStarterEl.addEventListener('click', function (event) {
-  event.stopPropagation()
+basketStarterEl.addEventListener('click', event => {
+  event.stopPropagation() // 이벤트 버블링 정지! - 버튼을 클릭했을 때 드롭다운 메뉴가 나타나야 함.
   if (basketEl.classList.contains('show')) {
-  //hide
-  hideBasket()
+    hideBasket()
   } else {
-  // show
-  showBasket()
+    showBasket()
   }
 })
-basketEl.addEventListener('click', function(event) {
-  event.stopPropagation()
+basketEl.addEventListener('click', event => {
+  event.stopPropagation() // 이벤트 버블링 정지! - 드롭다운 메뉴 영역을 클릭했을 때 메뉴가 사라지지 않아야 함.
 })
-
-window.addEventListener('click', function () {
+// 화면 전체를 클릭했을 때 메뉴가 사라짐.
+window.addEventListener('click', () => {
   hideBasket()
 })
 
+// 특정 로직을 직관적인 함수 이름으로 묶음.
 function showBasket() {
   basketEl.classList.add('show')
 }
@@ -30,7 +31,9 @@ function hideBasket() {
   basketEl.classList.remove('show')
 }
 
-// Search
+
+// 헤더 검색!
+// 헤더 검색 관련 요소 찾기.
 const headerEl = document.querySelector('header')
 const headerMenuEls = [...headerEl.querySelectorAll('ul.menu > li')]
 const searchWrapEl = headerEl.querySelector('.search-wrap')
@@ -39,65 +42,141 @@ const searchCloserEl = searchWrapEl.querySelector('.search-closer')
 const searchShadowEl = searchWrapEl.querySelector('.shadow')
 const searchInputEl = searchWrapEl.querySelector('input')
 const searchDelayEls = [...searchWrapEl.querySelectorAll('li')]
+const duration = .4 // 초(seconds) 단위, 시간을 변수에 저장해서 사용하면 쉽게 관리 용이
 
 searchStarterEl.addEventListener('click', showSearch)
-searchCloserEl.addEventListener('click', hideSearch)
+searchCloserEl.addEventListener('click', event => {
+  event.stopPropagation() // 데스크탑 레이아웃에서 클릭 이벤트가 버블링되어, 모바일 레이아웃에서 searchTextFieldEl가 클릭된 상태로 변하는 것을 방지
+  hideSearch()
+})
 searchShadowEl.addEventListener('click', hideSearch)
 
 function showSearch() {
   headerEl.classList.add('searching')
-  document.documentElement.classList.add('fixed')
-  headerMenuEls.reverse().forEach(function (el, index) {
-    el.style.transitionDelay = index * .4 / headerMenuEls.length + 's'
+  stopScroll()
+  headerMenuEls.reverse().forEach((el, index) => {
+    el.style.transitionDelay = `${index * duration / headerMenuEls.length}s` // 순서 * 지연 시간 / 애니메이션할 요소 개수
   })
-  searchDelayEls.forEach(function (el, index) {
-    el.style.transitionDelay = index * .4 / searchDelayEls.length + 's'
+  // .reverse() 사용하지 않고 원래 순서대로 반복 처리.
+  searchDelayEls.forEach((el, index) => {
+    el.style.transitionDelay = `${index * duration / searchDelayEls.length}s`
   })
-  setTimeout(function () {
+  // 검색 인풋 요소가 나타난 후 동작!
+  setTimeout(() => {
     searchInputEl.focus()
-  }, 600)
-  
+  }, 600);
 }
-
 function hideSearch() {
   headerEl.classList.remove('searching')
+  playScroll()
+  headerMenuEls.reverse().forEach((el, index) => {
+    el.style.transitionDelay = `${index * duration / headerMenuEls.length}s`
+  })
+  searchDelayEls.reverse().forEach((el, index) => {
+    el.style.transitionDelay = `${index * duration / searchDelayEls.length}s`
+  })
+  searchDelayEls.reverse() // 나타날 때 원래의 순서대로 처리해야 하기 때문에 다시 뒤집어서 순서 돌려놓기!
+  searchInputEl.value = '' // 입력값 초기화
+}
+function playScroll() {
+  // documentElement is <html>
   document.documentElement.classList.remove('fixed')
-  headerMenuEls.reverse().forEach(function (el, index) {
-    el.style.transitionDelay = index * .4 / headerMenuEls.length + 's'
-  })
-  searchDelayEls.reverse().forEach(function (el, index) {
-    el.style.transitionDelay = index * .4 / searchDelayEls.length + 's'
-  })
-  searchDelayEls.reverse()
-  searchInputEl.value = ''
+}
+function stopScroll() {
+  document.documentElement.classList.add('fixed')
 }
 
-// 요소의 가시성 관찰
-const io = new IntersectionObserver(function (entries) {
-  entries.forEach(function (entry) {
-    if(!entry.isIntersecting) {
+// 헤더 메뉴 토글! [모바일]
+const menuStarterEl = document.querySelector('header .menu-starter')
+menuStarterEl.addEventListener('click', () => {
+  if (headerEl.classList.contains('menuing')) {
+    headerEl.classList.remove('menuing')
+    searchInputEl.value = ''
+    playScroll()
+  } else {
+    headerEl.classList.add('menuing')
+    stopScroll()
+  }
+})
+
+// 헤더 검색! [모바일]
+const searchTextFieldEl = document.querySelector('header .textfield')
+const searchCancelEl = document.querySelector('header .search-canceler')
+searchTextFieldEl.addEventListener('click', () => {
+  headerEl.classList.add('searching--mobile')
+  searchInputEl.focus()
+})
+searchCancelEl.addEventListener('click', () => {
+  headerEl.classList.remove('searching--mobile')
+})
+
+// 화면 크기가 달라졌을 때 검색 모드가 종료되도록 처리.
+window.addEventListener('resize', event => {
+  if (window.innerWidth <= 740) {
+    headerEl.classList.remove('searching')
+  } else {
+    headerEl.classList.remove('searching--mobile')
+  }
+})
+
+
+// 네비게이션 메뉴 토글! [모바일]
+const navEl = document.querySelector('nav')
+const navMenuToggleEl = navEl.querySelector('.menu-toggler')
+const navMenuShadowEl = navEl.querySelector('.shadow')
+navMenuToggleEl.addEventListener('click', () => {
+  if (navEl.classList.contains('menuing')) {
+    hideNavMenu()
+  } else {
+    showNavMenu()
+  }
+})
+navEl.addEventListener('click', event => {
+  event.stopPropagation()
+})
+navMenuShadowEl.addEventListener('click', hideNavMenu)
+window.addEventListener('click', hideNavMenu)
+function showNavMenu() {
+  navEl.classList.add('menuing')
+}
+function hideNavMenu() {
+  navEl.classList.remove('menuing')
+}
+
+
+// 요소의 가시성 관찰 로직!
+const io = new IntersectionObserver(entries => {
+  // entries는 `io.observe(el)`로 등록된 모든 관찰 대상 배열.
+  entries.forEach(entry => {
+    // 사라질 때.
+    if (!entry.isIntersecting) {
       return
     }
-    entry.target.classList.add('show');
+    entry.target.classList.add('show')
   })
 })
+// 관찰할 요소들 검색
 const infoEls = document.querySelectorAll('.info')
-infoEls.forEach(function (el) {
-  io.observe(el)
-})
+// 관찰 시작!
+infoEls.forEach(el => io.observe(el))
 
 
-// 비디오 재생
+// 비디오 재생!
 const video = document.querySelector('.stage video')
 const playBtn = document.querySelector('.stage .controller--play')
 const pauseBtn = document.querySelector('.stage .controller--pause')
 
-playBtn.addEventListener('click', function() {
+// Google 자동 재생 정책 확인! - https://developer.chrome.com/blog/autoplay/#audiovideo-elements
+// video.play()
+//   .then(played)
+//   .catch(paused)
+
+playBtn.addEventListener('click', () => {
   video.play()
   playBtn.classList.add('hide')
   pauseBtn.classList.remove('hide')
 })
-pauseBtn.addEventListener('click', function() {
+pauseBtn.addEventListener('click', () => {
   video.pause()
   playBtn.classList.remove('hide')
   pauseBtn.classList.add('hide')
@@ -113,29 +192,30 @@ ipads.forEach(ipad => {
   let colorList = ''
   ipad.colors.forEach(color => {
     colorList += `<li style="background-color: ${color};"></li>`
-  })    
+  })
 
-// VS Code 확장 프로그램 - Comment tagged templates
-itemEl.innerHTML = /* html */ `
-<div class="thumbnail">
-  <img src="${ipad.thumbnail}" alt="${ipad.name}" />
-</div>
-<ul class="colors">
-  ${colorList}
-</ul>
-<h3 class="name">${ipad.name}</h3>
-<p class="tagline">${ipad.tagline}</p>
-<p class="price">₩${ipad.price.toLocaleString('en-US')}부터</p>
-<button class="btn">구입하기</button>
-<a href="${ipad.url}" class="link">더 알아보기</a>
-`
+  // VS Code 확장 프로그램 - Comment tagged templates
+  itemEl.innerHTML = /* html */ `
+    <div class="thumbnail">
+      <img src="${ipad.thumbnail}" alt="${ipad.name}" />
+    </div>
+    <ul class="colors">
+      ${colorList}
+    </ul>
+    <h3 class="name">${ipad.name}</h3>
+    <p class="tagline">${ipad.tagline}</p>
+    <p class="price">₩${ipad.price.toLocaleString('en-US')}부터</p>
+    <button class="btn">구입하기</button>
+    <a href="${ipad.url}" class="link">더 알아보기</a>
+  `
 
-itemsEl.append(itemEl)
+  itemsEl.append(itemEl)
 })
+
 
 // 푸터 내비게이션 맵 랜더링!
 const navigationsEl = document.querySelector('footer .navigations')
-  navigations.forEach(nav => {
+navigations.forEach(nav => {
   const mapEl = document.createElement('div')
   mapEl.classList.add('map')
 
@@ -159,9 +239,11 @@ const navigationsEl = document.querySelector('footer .navigations')
   navigationsEl.append(mapEl)
 })
 
+
 // 올해 연도를 적용!
 const thisYearEl = document.querySelector('.this-year')
 thisYearEl.textContent = new Date().getFullYear()
+
 
 // 푸터 내비게이션 맵 아코디언
 const mapEls = [...document.querySelectorAll('footer .navigations .map')]
